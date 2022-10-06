8c2d90f496e8a51cb76c14c4aa19102f7efc39e9
KAUANSANTOS <66709503+KAUANPEREIRA@users.noreply.github.com>
2022-10-06 16:03:25 -0300

    Create AboutPedro.tsx

---
+++ src/pages/AboutPedro.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const AboutPedro = () => {
+  return (
+    <div>
+        Pagina about
+        <h1>Sobre Pedro</h1>
+        <p>Pedro um jogador de futebol atacante bom de bola</p>
+        
+    </div>
+  )
+}
